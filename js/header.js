fc483e83ed595d8fa2e16b30c981910a8cb143de
AidanB1982 <aidan@blackwoodpublishing.co.uk>
2026-04-06 19:57:48 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,1 +1,26 @@
+// =========================
+// LOAD HEADER
+// =========================
 
+async function loadHeader(type = "hero") {
+
+    let file = "header.html";
+
+    if (type === "simple") {
+        file = "header-simple.html";
+    }
+
+    try {
+        const res = await fetch(file);
+        const html = await res.text();
+
+        const container = document.getElementById("header-placeholder");
+
+        if (container) {
+            container.innerHTML = html;
+        }
+
+    } catch (err) {
+        console.error("Header load failed:", err);
+    }
+}
