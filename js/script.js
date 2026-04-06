dcf04d25a500798921463ca9f1f6c3db19cc64ed
AidanB1982 <aidan@blackwoodpublishing.co.uk>
2026-04-06 19:44:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
-<script>
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-        }
-    });
-}, { threshold: 0.2 });
 
-document.querySelectorAll('.book').forEach(el => observer.observe(el));
-</script>
